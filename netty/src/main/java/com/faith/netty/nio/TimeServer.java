5df13378dabdd35f4289bbeb024629eb54d46616
Admin <183120084@qq.com>
2017-08-24 00:25:08 +0800

    netty nio

---
+++ netty/src/main/java/com/faith/netty/nio/TimeServer.java
@@ -0,0 +1,43 @@
+package com.faith.netty.nio;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+
+    public void bind(int port) {
+        //配置服务端的NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            //绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+            //等待服务端
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast((ChannelHandler) new Object());
+        }
+    }
+
+    public static void main(String[] args) {
+        new TimeServer().bind(8080);
+    }
+}
