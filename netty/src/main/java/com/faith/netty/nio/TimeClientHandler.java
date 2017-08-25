3e92ec3b2905e425b64e9b35651a5355bbca5316
yunyun <guozhongping@aixuedai.com>
2017-08-25 12:35:30 +0800

    粘包  错误示例

--- netty/src/main/java/com/faith/netty/nio/TimeClientHandler.java
+++ netty/src/main/java/com/faith/netty/nio/TimeClientHandler.java
@@ -7,17 +7,23 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class TimeClientHandler extends ChannelInboundHandlerAdapter {
 
-    private final ByteBuf firstMessage;
+    private ByteBuf firstMessage;
+
+    private int counter;
+    private byte[] req;
 
     public TimeClientHandler() {
-        byte[] req = "QUERY TIME ORDER".getBytes();
-        this.firstMessage = Unpooled.buffer(req.length);
-        firstMessage.writeBytes(req);
+        req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
-        ctx.writeAndFlush(firstMessage);
+        for (int i = 0; i < 100; i++) {
+            this.firstMessage = Unpooled.buffer(req.length);
+            firstMessage.writeBytes(req);
+            ctx.writeAndFlush(firstMessage);
+        }
+
     }
 
     @Override
@@ -26,7 +32,7 @@ public class TimeClientHandler extends ChannelInboundHandlerAdapter {
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
         String body = new String(req, "utf-8");
-        System.out.println("now is : " + body);
+        System.out.println("now is : " + body + " ; the counter is : " + ++counter);
     }
 
     @Override
--- netty/src/main/java/com/faith/netty/nio/TimeServerHandler.java
+++ netty/src/main/java/com/faith/netty/nio/TimeServerHandler.java
@@ -4,28 +4,38 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.w3c.dom.css.Counter;
 
 public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    private int counter;
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf buf = (ByteBuf) msg;
         byte[] req = new byte[buf.readableBytes()];
         buf.readBytes(req);
-        String body = new String(req, "utf-8");
-        System.out.println("the time server receive order : " + body);
-        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
-        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-        ctx.write(resp);
-    }
 
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
+        //sample case
+//        String body = new String(req, "utf-8");
+//        System.out.println("the time server receive order : " + body);
+//        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
+//                new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+
+        //halfpackage case
+        String body = new String(req, "utf-8").substring(0, req.length - System.getProperty("line.separator").length());
+        System.out.println("the time server receive order : " + body + " ; the counter is : " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
+                new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime += System.getProperty("line.separator");
+
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         ctx.close();
     }
+
 }
